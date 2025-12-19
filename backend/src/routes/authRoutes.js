a90e1f85d5fcb9dcb0d0f8b9c6a2fe8a1392c660
Yashwanth N <yashalex03@gmail.com>
2025-12-19 20:50:42 +0530

    changed authRouter.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,4 @@
 import express from "express";
-
-import bcrypt from "bcrypt";
-import { jwtSecret } from "../config/processEnv.js";
-import jwt from "jsonwebtoken";
-import { signUpSchema } from "../validators/authSchema.js";
-import { UserModel } from "../models/User.js";
 import { signUp, signIn } from "../controllers/authController.js";
 const authRouter = express.Router();
 
