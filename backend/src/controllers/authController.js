71cca61ef5d2b418b161ab013d4b056ee9cf7c95
Yashwanth N <yashalex03@gmail.com>
2025-12-19 20:48:08 +0530

    moved the auth related logic to authController

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,48 @@
+import { UserModel } from "../models/User.js";
+import { signUpSchema } from "../validators/authSchema.js";
+import { jwtSecret } from "../config/processEnv.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const signUp = async (req, res) => {
+  const parsedData = signUpSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    return res
+      .status(411)
+      .json({ message: "Incorrect Format", error: parsedData.error });
+    const { fullname, email, password } = parsedData.data;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await UserModel.create({
+        fullname,
+        email,
+        password: hashedPassword,
+      });
+      res.json({ message: "You have signed up! Please proceed to sign in." });
+    } catch (err) {
+      if (err.code === 11000) {
+        res.status(409).json({ message: "User already exists." });
+      } else {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    }
+  }
+};
+
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  const response = await UserModel.findOne({ email });
+  if (!response) {
+    return res.status(403).json({ message: "Invalid credentials" });
+  }
+  const passwordMatch = await bcrypt.compare(password, response.password);
+  if (passwordMatch) {
+    const token = jwt.sign({ id: response._id.toString() }, jwtSecret, {
+      expiresIn: "1d",
+    });
+    return res.json({ token });
+  } else {
+    return res.status(403).json({ message: "Invalid credentials" });
+  }
+};
+
+export { signUp, signIn };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,52 +5,11 @@ import { jwtSecret } from "../config/processEnv.js";
 import jwt from "jsonwebtoken";
 import { signUpSchema } from "../validators/authSchema.js";
 import { UserModel } from "../models/User.js";
+import { signUp, signIn } from "../controllers/authController.js";
 const authRouter = express.Router();
 
-authRouter.post("/sign-up", async (req, res) => {
-  const parsedData = signUpSchema.safeParse(req.body);
-  if (!parsedData.success) {
-    //status code 411 - for bad input
-    return res
-      .status(411)
-      .json({ message: "Incorrect Format", error: parsedData.error });
-  }
-  const { fullname, email, password } = parsedData.data;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await UserModel.create({
-      fullname,
-      email,
-      password: hashedPassword,
-    });
-    res.json({ message: "You have signed up! Please proceed to sign in." });
-  } catch (err) {
-    // status code 409 - conflict - duplicate
-    if (err.code === 11000) {
-      res.status(409).json({ message: "User already exists." });
-    } else {
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  }
-});
+authRouter.post("/sign-up", signUp);
 
-authRouter.post("/sign-in", async (req, res) => {
-  const { email, password } = req.body;
-  const response = await UserModel.findOne({ email });
-  if (!response) {
-    // 403 - forbidden request
-    return res.status(403).json({ message: "Invalid credentials" });
-  }
-
-  const passwordMatch = await bcrypt.compare(password, response.password);
-  if (passwordMatch) {
-    const token = jwt.sign({ id: response._id.toString() }, jwtSecret, {
-      expiresIn: "1d",
-    });
-    return res.json({ token });
-  } else {
-    return res.status(403).json({ message: "Invalid credentials" });
-  }
-});
+authRouter.post("/sign-in", signIn);
 
 export { authRouter };
